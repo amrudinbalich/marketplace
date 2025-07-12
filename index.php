df5baa36e38126ba9c0dd3b9b648b6731b3df208
Amrudin Balić <balicamrudin@gmail.com>
2025-07-12 19:49:16 +0200

    redirecting to public/index.php - scaffolding

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
-require_once 'vendor/autoload.php';
+// currently cannot setup .htaccess to redirect to public/index.php
+// so we are redirecting to it manually
+
+header('Location: public/index.php');
+exit;
